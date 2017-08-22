faba92034e1dfa4b289de477a4fe59f1b9dd8e61
Landay <1curt20@gmail.com>
2017-08-22 13:48:54 +0300

    first commit

---
+++ Parser/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Parser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.btnOpenDir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(189, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 600);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(9, 227);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 10;
+            this.dataGridView1.Size = new System.Drawing.Size(175, 373);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(9, 203);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Create file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.FormattingEnabled = true;
+            this.lstFiles.Location = new System.Drawing.Point(9, 12);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(177, 186);
+            this.lstFiles.TabIndex = 4;
+            this.lstFiles.SelectedIndexChanged += new System.EventHandler(this.lstFiles_SelectedIndexChanged);
+            // 
+            // btnOpenDir
+            // 
+            this.btnOpenDir.Location = new System.Drawing.Point(368, 9);
+            this.btnOpenDir.Name = "btnOpenDir";
+            this.btnOpenDir.Size = new System.Drawing.Size(167, 21);
+            this.btnOpenDir.TabIndex = 1;
+            this.btnOpenDir.Text = "Open Dir";
+            this.btnOpenDir.UseVisualStyleBackColor = true;
+            this.btnOpenDir.Click += new System.EventHandler(this.btnOpenDir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(541, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "You selected a folder: ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Gel Loreal",
+            "Gel Gillete",
+            "Gel Nivea ",
+            "Nivea1",
+            "Nivea2",
+            "Rinser",
+            "Palmolive1",
+            "Palmolive2",
+            "Palmolive3",
+            "Palmolive4",
+            "Palmolive5",
+            "Palmolive6",
+            "Gohmonis"});
+            this.comboBox1.Location = new System.Drawing.Point(189, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(166, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(128, 203);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label2.Location = new System.Drawing.Point(0, 634);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(992, 460);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstFiles);
+            this.Controls.Add(this.btnOpenDir);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox lstFiles;
+        private System.Windows.Forms.Button btnOpenDir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Parser/Form1.cs
@@ -0,0 +1,457 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Compression;
+
+namespace Parser
+{
+
+    public partial class Form1 : Form
+    {
+        public Point ptbeg, ptend;
+        public bool draw = false;
+        public string selectedDir;
+        List<Point> p;
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Paint += new PaintEventHandler(this.pictureBox1_Paint);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+            ptbeg = ptend = Point.Empty;
+            dataGridView1.ColumnCount = 5;
+            dataGridView1.Columns[0].HeaderText = "x1";
+            dataGridView1.Columns[1].HeaderText = "y1";
+            dataGridView1.Columns[2].HeaderText = "x2";
+            dataGridView1.Columns[3].HeaderText = "y2";
+            dataGridView1.Columns[4].HeaderText = "o";
+
+            foreach (DataGridViewColumn c in dataGridView1.Columns)
+            {
+                c.AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+                c.Width = 32;
+            }
+            dataGridView1.Columns[4].Width = 25;
+
+
+
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            dataGridView1.RowsRemoved += (o, e) => { pictureBox1.Invalidate(); };
+            //dataGridView1.DataSource = p;
+            string file = "classes.txt";
+            comboBox1.Items.Clear();
+            if (File.Exists(file))
+            {
+                var lines = File.ReadAllLines(file);
+                foreach (string s in lines)
+                {
+                    comboBox1.Items.Add(s);
+                }
+            }
+            comboBox1.SelectedIndex = 0;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // dataGridView1.Rows.Add(row6);
+
+            var ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+                pictureBox1.Image = Image.FromFile(ofd.FileName);
+
+        }
+
+        private void pictureBox1_Paint1(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            // use g to do your drawing
+            Color cl = new Color();
+            Brush br = new SolidBrush(ForeColor);
+            // g.DrawRectangle(new Pen(br),0,0,100,100);
+            //alert("aaaa");
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                DataGridViewRow row = dataGridView1.Rows[i];
+                //   MessageBox.Show(row.Cells[0].Value.ToString());
+                int index = Int32.Parse(row.Cells[4].Value.ToString());
+                Array colorsArray = Enum.GetValues(typeof(KnownColor));
+                KnownColor[] allColors = new KnownColor[colorsArray.Length];
+
+                Array.Copy(colorsArray, allColors, colorsArray.Length);
+                //br = new SolidBrush(Color.FromArgb(index == 0 ? 255 : 0, index == 1 ? 255 : 0, index == 2 ? 255 : 0));
+                br = new SolidBrush(Color.FromName(allColors[index*3].ToString()));
+                float x = Int32.Parse(row.Cells[0].Value.ToString());
+                float y = Int32.Parse(row.Cells[1].Value.ToString());
+                float w = Int32.Parse(row.Cells[2].Value.ToString());
+                float h = Int32.Parse(row.Cells[3].Value.ToString());
+                g.DrawRectangle(new Pen(br, 2), x, y, Math.Abs(w - x), Math.Abs(h - y));
+                g.DrawString(comboBox1.Items[index].ToString(), new Font(FontFamily.GenericSansSerif,
+            12.0F, FontStyle.Bold), br, x, y - 20);
+            }
+
+        }
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+
+            if (keyData == Keys.Enter)
+            {
+                createFile();
+            }
+            //else if(keyData == Keys.Down)
+            //{
+            //    if(comboBox1.Items.Count - 2 > comboBox1.SelectedIndex)
+            //        comboBox1.SelectedIndex = comboBox1.SelectedIndex + 1;
+            //}
+            //else if (keyData == Keys.Up)
+            //{
+            //    if (0 < comboBox1.SelectedIndex)
+            //        comboBox1.SelectedIndex = comboBox1.SelectedIndex - 1;
+            //}
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void Down(Point a)
+        {
+            ptbeg = ptend = a;
+        }
+
+
+
+        private void pictureBox1_MouseMove1(object sender, MouseEventArgs e)
+        {
+            if (!draw) return;
+            Graphics grfx = pictureBox1.CreateGraphics();
+            grfx.DrawRectangle(new Pen(BackColor), ptbeg.X, ptbeg.Y,
+                ptend.X - ptbeg.X, ptend.Y - ptbeg.Y);
+            ptend = new Point(e.X, e.Y);
+            pictureBox1.Invalidate();
+            grfx.DrawRectangle(new Pen(ForeColor), ptbeg.X, ptbeg.Y,
+                ptend.X - ptbeg.X, ptend.Y - ptbeg.Y);
+            grfx.Dispose();
+        }
+        public void alert(object Show)
+        {
+            MessageBox.Show(Show.ToString());
+        }
+        public void createFile()
+        {
+            string fileName = lstFiles.SelectedItem.ToString();
+            fileName = fileName.Substring(0, fileName.Length - 3);
+            string path = selectedDir + "\\" + fileName + "txt";
+            //  MessageBox.Show(path);
+            //  return;
+            
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(path))
+            {
+                MessageBox.Show(dataGridView1.Rows.Count.ToString());
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    DataGridViewRow row = dataGridView1.Rows[i];
+                    //   MessageBox.Show(row.Cells[0].Value.ToString());
+                    double x = Int32.Parse(row.Cells[0].Value.ToString());
+                    double y = Int32.Parse(row.Cells[1].Value.ToString());
+                    double w = Int32.Parse(row.Cells[2].Value.ToString());
+                    double h = Int32.Parse(row.Cells[3].Value.ToString());
+                    if (x > w)
+                    {
+                        double temp = x;
+                        x = w;
+                        w = temp;
+                    }
+                    if (y > h)
+                    {
+                        double temp = y;
+                        y = h;
+                        h = temp;
+                    }
+                    // int full_w = pictureBox1.Image.Size.Width;
+                    // int full_h = pictureBox1.Image.Size.Height;
+                    int full_w = pictureBox1.ClientSize.Width;
+                    int full_h = pictureBox1.ClientSize.Height;
+                    string line = row.Cells[4].Value.ToString() + " " +
+                        ((x + w) / (2 * full_w)).ToString().Replace(",", ".") + " " +
+                        ((y + h) / (2 * full_h)).ToString().Replace(",", ".") + " " +
+                        ((w - x) / full_w).ToString().Replace(",", ".") + " " +
+                        ((h - y) / full_h).ToString().Replace(",", ".")
+                        ;
+                    // MessageBox.Show(line);
+                    //len += info.Length;
+                    file.WriteLine(line);
+                    // Add some information to the file.
+                    //fs.WriteLine(info, len, info.Length);
+                    //fs.Write
+                }
+            }
+            if (lstFiles.SelectedIndex < lstFiles.Items.Count)
+                lstFiles.SelectedIndex = lstFiles.SelectedIndex + 1;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            createFile();
+
+
+        }
+
+        private void btnOpenDir_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    selectedDir = fbd.SelectedPath;
+                    label1.Text += selectedDir;
+                    DirectoryInfo di = new DirectoryInfo(fbd.SelectedPath);
+                    foreach (FileInfo fileInfo in di.EnumerateFiles())
+                    {
+                        if (fileInfo.Extension == ".png" || fileInfo.Extension == ".jpg" || fileInfo.Extension == ".JPG"
+                            || fileInfo.Extension == ".jpeg" || fileInfo.Extension == ".JPEG" || fileInfo.Extension == ".PNG")
+                        {
+                            lstFiles.Items.Add(fileInfo.Name);
+                        }
+                    }
+                }
+            }
+        }
+
+        private float GetValueFromString(string s)
+        {
+            return float.Parse(s, System.Globalization.NumberStyles.Number, System.Globalization.NumberFormatInfo.InvariantInfo);
+        }
+
+
+        private void lstFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var filePath = selectedDir + "\\" + lstFiles.SelectedItem;
+
+            pictureBox1.Image = Image.FromFile(filePath);
+            //pictureBox1.Size = pictureBox1.Image.Size;
+            //alert(pictureBox1.Image.Size.Width);
+            //alert(pictureBox1.ClientSize.Width);
+            dataGridView1.Rows.Clear();
+            comboBox1.SelectedIndex = 0;
+            
+            try {
+
+
+                string fileName = lstFiles.SelectedItem.ToString();
+                fileName = fileName.Substring(0, fileName.Length - 3);
+                string path = selectedDir + "\\" + fileName + "txt";
+
+                if (File.Exists(path)) {
+
+                    int full_w = pictureBox1.ClientSize.Width;
+                    int full_h = pictureBox1.ClientSize.Height;
+
+
+                    var lines = File.ReadAllLines(path);
+                    foreach (string s in lines) {
+                        var items = s.Split(' ');
+                        float c1 = GetValueFromString(items[1]);
+                        float c2 = GetValueFromString(items[2]);
+                        float c3 = GetValueFromString(items[3]);
+                        float c4 = GetValueFromString(items[4]);
+
+
+                        int w = (int)Math.Round((2 * c1 + c3 ) * full_w/2 );
+                        int x = (int)Math.Round((2 * c1 - c3 ) * full_w/2);
+                        int h = (int)Math.Round((2 * c2 + c4 ) * full_h/2 );
+                        int y = (int)Math.Round((2 * c2  - c4 ) * full_h/2 );
+
+                        dataGridView1.Rows.Add(x, y, w, h, Convert.ToInt32(items[0]));
+
+                        if ((((x > full_w) || (w > full_w)) || ((x < 0) || (w < 0)))||
+                                (((y > full_h) || (h > full_h)) || ((y < 0) || (h < 0))))
+                        {
+                            MessageBox.Show("Your selection does not fit picture area.");
+                            pictureBox1.Invalidate();
+                        }
+                         /*
+                         x + w = c1 * 2 * full_w
+                         w - x = c3*full_w
+                         y + h  =2 *c2 * full_h
+                         h - y  =c4*full_h
+                         */
+                    };
+                }
+            }
+            catch(Exception ex)  { MessageBox.Show(ex.Message); }
+
+            label2.Text ="w:" + pictureBox1.ClientSize.Width+
+            " x h:" + pictureBox1.ClientSize.Height;
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseUp1(object sender, MouseEventArgs e)
+        {
+            draw = false;
+            
+            string[] column = {
+                    ptbeg.X.ToString(),
+                    ptbeg.Y.ToString(),
+                    ptend.X.ToString(),
+                    ptend.Y.ToString(),
+                    comboBox1.SelectedIndex.ToString()
+                };
+            dataGridView1.Rows.Add(column);
+
+            pictureBox1.Invalidate();
+            ptbeg = new Point(e.X, e.Y);
+        }
+
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            return;
+            if (draw == true)
+            {
+                string[] column = {
+                    ptbeg.X.ToString(),
+                    ptbeg.Y.ToString(),
+                    ptend.X.ToString(),
+                    ptend.Y.ToString(),
+                    comboBox1.SelectedIndex.ToString()
+                };
+                dataGridView1.Rows.Add(column);
+            }
+            draw = !draw;
+            pictureBox1.Invalidate();
+            ptbeg = new Point(e.X, e.Y);
+        }
+
+
+        /*****************************************/
+
+        private Point RectStartPoint;
+        private Rectangle Rect = new Rectangle();
+        private Brush selectionBrush = new SolidBrush(Color.FromArgb(128, 72, 145, 220));
+
+        // Start Rectangle
+        //
+        private void pictureBox1_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (pictureBox1.Image == null) return;
+            // Determine the initial rectangle coordinates...
+            RectStartPoint = e.Location;
+            //Invalidate();
+        }
+
+        // Draw Rectangle
+        //
+        private void pictureBox1_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (pictureBox1.Image == null) return;
+            if (e.Button != MouseButtons.Left)
+                return;
+            Point tempEndPoint = e.Location;
+            Rect.Location = new Point(
+                Math.Min(RectStartPoint.X, tempEndPoint.X),
+                Math.Min(RectStartPoint.Y, tempEndPoint.Y));
+            Rect.Size = new Size(
+                Math.Abs(RectStartPoint.X - tempEndPoint.X),
+                Math.Abs(RectStartPoint.Y - tempEndPoint.Y));
+            pictureBox1.Invalidate();
+        }
+        // Draw Area
+        //
+        private void pictureBox1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            // Draw the rectangle...
+            if (pictureBox1.Image != null)
+            {
+                if (Rect != null && Rect.Width > 0 && Rect.Height > 0)
+                {
+                    e.Graphics.FillRectangle(selectionBrush, Rect);
+                }
+                pictureBox1_Paint1(sender, e);
+            }
+        }
+
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            pictureBox1.Invalidate();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string startPath = @"c:\example\start";
+            string zipPath = @"c:\example\result.zip";
+            string extractPath = @"c:\example\extract";
+            
+            //ZipFile.CreateFromDirectory(startPath, zipPath);
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (pictureBox1.Image == null) return;
+            if (e.Button == MouseButtons.Left)
+            {
+                
+                var tempEndPoint = e.Location;
+                object[] column = {
+                    Math.Max(Math.Min(RectStartPoint.X, tempEndPoint.X),0),
+                    Math.Max(Math.Min(RectStartPoint.Y, tempEndPoint.Y),0),
+                    Math.Min(Math.Max(RectStartPoint.X, tempEndPoint.X),pictureBox1.ClientSize.Width),
+                    Math.Min(Math.Max(RectStartPoint.Y, tempEndPoint.Y), pictureBox1.ClientSize.Height),
+                    comboBox1.SelectedIndex
+                };
+                dataGridView1.Rows.Add(column);
+                Rect = new Rectangle();
+                pictureBox1.Invalidate();
+
+                if ((int)column[0] == 0 && (int)column[1] == 0)
+                    MessageBox.Show("Check rectangle at the left top corner. Is it error?");
+
+            }
+
+
+            if (e.Button == MouseButtons.Right)
+            {
+                /*int i = dataGridView1.Rows.Count;
+                while (i > 0)
+                {
+
+
+                }*/
+
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    DataGridViewRow row = dataGridView1.Rows[i];
+                    int x = Int32.Parse(row.Cells[0].Value.ToString());
+                    int y = Int32.Parse(row.Cells[1].Value.ToString());
+                    int w = Int32.Parse(row.Cells[2].Value.ToString());
+                    int h = Int32.Parse(row.Cells[3].Value.ToString());
+
+                    Rectangle r = new Rectangle(x, y, Math.Abs(w - x), Math.Abs(h - y));
+                    if (r.Contains(e.Location))
+                    {
+                        dataGridView1.Rows.Remove(row);
+                        break;
+                    }
+                }
+            }
+        }
+
+
+    }
+}
+
